5097c6111f0283d6ad68a8462fd8c69fd58cf844
thiago <thiagooffice2@gmail.com>
2022-04-21 18:35:09 -0300

    initial routes configuration

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApyjCsC244-aaY9JTrdZoRjjFRmflnOj4",
+  authDomain: "miniblog-13d65.firebaseapp.com",
+  projectId: "miniblog-13d65",
+  storageBucket: "miniblog-13d65.appspot.com",
+  messagingSenderId: "398801987025",
+  appId: "1:398801987025:web:60648f647d4d5985566cf0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export {db}
