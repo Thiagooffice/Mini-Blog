3f6fec8ddf5ac2065a58a0ba4a21616e858a9367
thiago <thiagooffice2@gmail.com>
2022-05-02 22:41:23 -0300

    add-user-to-context

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Footer from './components/Footer'
 
 //Contetx
 import { AuthProvider } from './context/AuthContext';
+import { onAuthStateChanged,  } from 'firebase/auth';
+
+//hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
 
 //pages
 import Home from './pages/Home/Home'
@@ -15,9 +20,27 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(()=>{
+
+    onAuthStateChanged(auth, (user)=>{
+      setUser(user)
+    })
+
+  },[auth])
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{user}}>
         <BrowserRouter>
           <NavBar />
           <div className="container">
