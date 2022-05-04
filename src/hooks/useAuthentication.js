dbb677fe827a82465e84a7f8c8b63c28274e80ba
thiago <thiagooffice2@gmail.com>
2022-05-03 21:25:10 -0300

    create-post-page-structure

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -4,7 +4,8 @@ import {
     createUserWithEmailAndPassword,
     signWithEmailAndPassword,
     updateProfile,
-    signOut
+    signOut,
+    signInWithEmailAndPassword
 } from 'firebase/auth'
 
 import {useState, useEffect} from 'react'
@@ -74,6 +75,34 @@ export const useAuthentication = () => {
 
     }
 
+    //login -sign in
+    const login = async (data)=>{
+
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(false)
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false)
+        } catch (error) {
+
+            let systemErrorMessage
+
+            if(error.message.includes("user-not-found")){
+                systemErrorMessage = "Usuário não encontrado."
+            } else if(error.message.includes("wrong-password")){
+                systemErrorMessage = "Senha incorreta."
+            } else{
+                systemErrorMessage = "Ocorreu um erro, por favor, tente mais tarde."
+            }
+            setError(systemErrorMessage)
+            setLoading(false)
+        }
+
+    }
+
     useEffect(()=>{
         return ()=> setCancelled(true)
     },[])
@@ -84,6 +113,7 @@ export const useAuthentication = () => {
         error,
         loading,
         logout,
+        login,
     }
 
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -8,7 +8,7 @@ export default function Login(){
     const [password, setPassword]=useState("")
     const [error,setError]=useState("")
 
-    const {createUser, error: authError, loading} = useAuthentication()
+    const {login, error: authError, loading} = useAuthentication()
 
     const handleSubmit = async (e)=>{
         e.preventDefault()
@@ -20,7 +20,7 @@ export default function Login(){
             password
         }
 
-        const res = await createUser(user)
+        const res = await login(user)
 
         console.log(res)
     }
